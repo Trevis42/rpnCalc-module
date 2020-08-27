960dd19ddcd12643ef3efad3440d58e3a9f8f630
Trevis42 <sirtrevis42@gmail.com>
2020-08-27 15:00:45 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# What is this?
+
+### Reverse Polish Notation Calculator:
+
+Supports `+ - * /`
+
+# Installation
+
+`npm install rpn-calculator --save`
+
+Then
+
+```javascript
+import {rpn} from 'rpn-calculator';
+
+console.log(rpn(2 1 +));
+
+//output: 3
+```
---
+++ index.js
@@ -0,0 +1,56 @@
+// Reverse Polish Notation Calculator:
+// supports + _ * /
+const rpn = (expression) => {
+	let result = 0;
+	let stack = [];
+	let expArr = [];
+	if (expression === undefined || expression === null) {
+		return 0;
+	} else {
+		expArr = expression.split(" ");
+	}
+	for (const v of expArr) {
+		//console.log(typeof Number.parseInt(v));
+		stack.push(v);
+		if (isNaN(parseInt(v))) {
+			stack.pop();
+			if (v === "+") {
+				//addition
+				let var2 = stack.pop();
+				let var1 = stack.pop();
+				let preResult = Number(var1) + Number(var2);
+				stack.push(preResult);
+			}
+			if (v === "-") {
+				//subtraction
+				let var2 = stack.pop();
+				let var1 = stack.pop();
+				let preResult = Number(var1) - Number(var2);
+				stack.push(preResult);
+			}
+			if (v === "*") {
+				//multiplication
+				let var2 = stack.pop();
+				let var1 = stack.pop();
+				let preResult = Number(var1) * Number(var2);
+
+				stack.push(preResult);
+			}
+			if (v === "/") {
+				//division
+				let var2 = stack.pop();
+				let var1 = stack.pop();
+				let preResult = Number(var1) / Number(var2);
+				stack.push(preResult);
+			}
+		}
+	}
+	if (stack.length > 1) {
+		return "Expression invalid (too many remaining items in stack)";
+	} else {
+		result = stack.pop();
+	}
+	return result;
+};
+
+export default rpn;
